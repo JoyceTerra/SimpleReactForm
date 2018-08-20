17a9ddb675c4e9689211d99e0967b34b1dc68317
Arien Kock <arien@positor.nl>
2018-08-20 16:36:31 +0200

    routing boilerplate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
-import { Beatle } from './Beatle'
 import './App.css';
+import books from './books'
 
 class App extends Component {
-  state = {
-    names: []
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({names: ["Paul", "John", "George", "Ringo"]})
-    }, 1000)
-  }
-
   render() {
     return (
-      <div className="App" style={{color: 'red'}}>
-        {this.state.names.map(name => <Beatle name={name} />)}
+      <div className="App">
+        <nav>
+          <ul>
+            {books.map(book => <li key={book.isbn}>{book.title}</li>)}
+          </ul>
+        </nav>
+        <main>
+          Put the details here...
+        </main>
       </div>
     );
   }
