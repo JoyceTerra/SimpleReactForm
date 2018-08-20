7174f72698c0f48886a1e37634bc8b7406e10ad0
Arien Kock <arien@positor.nl>
2018-08-20 21:30:53 +0200

    container component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import './App.css';
-import books from './books'
 
 class App extends Component {
+  state = {
+    imageUrl: ''
+  }
+
+  componentDidMount() {
+    this.loadNew()
+  }
+
+  loadNew = () => {
+    this.setState({ imageUrl: '' })
+    fetch('https://aws.random.cat/meow').then(r => r.json()).then(({ file }) => {
+      this.setState({ imageUrl: file })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <nav>
-          <ul>
-            {books.map(book => <li key={book.isbn}>{book.title}</li>)}
-          </ul>
-        </nav>
-        <main>
-          Put the details here...
-        </main>
+      <div className="App" onClick={this.loadNew}>
+        {!this.state.imageUrl && <div>Loading...</div>}
+        {this.state.imageUrl &&
+          <img
+            src={this.state.imageUrl}
+            style={{ maxWidth: '300px', cursor: 'pointer' }}
+            alt="a random cat" />
+        }
       </div>
     );
   }
