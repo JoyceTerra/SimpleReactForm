6db9d35fc55343df4717cc8608e86876a6ee51c8
JoyceTerra <joyterra@gmail.com>
2018-08-22 13:39:45 +0200

    Name input finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {value: ' '}
+  
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+
+  handleChange(event){
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event){
+    alert(`A name was submitted: ${this.state.value}`)
+    event.preventDefault();
+
+  }
+  
   render() {
     return (
-      <form className="App">
-        <div><label>Name: <input name="name" /></label></div>
+      <form className="App" onSubmit={this.handleSubmit}>
+        <div><label>Name: <input name="name"  value={this.state.value} onChange={this.handleChange}/></label></div>
         <div><label>Date of birth: <input name="dateofbirth" type="date" /></label></div>
         <div>
           <label>Gender: 
